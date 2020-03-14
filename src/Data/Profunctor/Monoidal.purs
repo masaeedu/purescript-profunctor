23935f407a2d970a5bd2ec31bac7d7237d154652
Asad Saeeduddin <masaeedu@gmail.com>
2020-03-14 03:42:58 -0400

    Refactor Semigroupal and Monoidal

--- src/Data/Profunctor/Monoidal.purs
+++ src/Data/Profunctor/Monoidal.purs
@@ -1,14 +1,55 @@
 module Data.Profunctor.Monoidal where
 
+import Prelude
+
+import Data.Bifunctor (class Bifunctor)
+import Data.Either (Either(..), either)
 import Data.Profunctor (class Profunctor)
+import Data.Tuple (Tuple(..), fst, snd)
+import Data.Tuple.Nested ((/\))
 
-class Profunctor p <= Semigroupal s t p
-  where
-  zip ::
-    forall a b c d.
-    t (p a b) (p c d) -> p (s a c) (s b d)
+type Iso p a b = { fwd :: p a b, bwd :: p b a }
 
-class Semigroupal s t p <= Monoidal s t i j p
-  | s p -> i, t p -> j, i p -> s, j p -> t
+class Bifunctor t <= Associative t p
   where
-  unit :: j -> p i i
+  assoc :: forall a b c. Iso p (t a (t b c)) (t (t a b) c)
+
+instance associativeCartesian :: Associative Tuple (->)
+  where
+  assoc =
+    { fwd: \(a /\ b /\ c) -> (a /\ b) /\ c
+    , bwd: \((a /\ b) /\ c) -> a /\ b /\ c
+    }
+
+instance associativeCocartesian :: Associative Either (->)
+  where
+  assoc =
+    { fwd: either (Left <<< Left) (either (Left <<< Right) Right)
+    , bwd: either (either Left (Right <<< Left)) (Right <<< Right)
+    }
+
+class Associative t p <= Tensor t i p
+  where
+  lunit :: forall a. Iso p (t i a) a
+  runit :: forall a. Iso p (t a i) a
+
+instance tensorCartesian :: Tensor Tuple Unit (->)
+  where
+  lunit = { fwd: snd, bwd: Tuple unit }
+  runit = { fwd: fst, bwd: flip Tuple unit }
+
+instance tensorCocartesian :: Tensor Either Void (->)
+  where
+  lunit = { fwd: either absurd identity, bwd: Right }
+  runit = { fwd: either identity absurd, bwd: Left }
+
+class (Associative l c, Associative r c, Profunctor p) <= Semigroupal c l r o p
+  where
+  pzip :: forall t u v w.
+    c (o (p t u) (p v w)) (p (l t v) (r u w))
+
+class Profunctor p <= Unital c l r o p
+  where
+  punit :: c o (p l r)
+
+class (Semigroupal c l r o p, Unital c il ir io p) <= Monoidal c l il r ir o io p
