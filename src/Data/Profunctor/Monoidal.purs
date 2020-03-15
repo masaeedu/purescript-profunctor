2671876cc8ea6a37c379bff427b2dfd8430f72ae
Asad Saeeduddin <masaeedu@gmail.com>
2020-03-15 02:43:46 -0400

    Move monoidal structure into separate module

--- src/Data/Profunctor/Monoidal.purs
+++ src/Data/Profunctor/Monoidal.purs
@@ -1,57 +1,18 @@
 module Data.Profunctor.Monoidal where
 
-import Prelude
-
-import Data.Bifunctor (class Bifunctor)
-import Data.Either (Either(..), either)
+import Control.Category.Tensor (class Associative)
 import Data.Profunctor (class Profunctor)
-import Data.Tuple (Tuple(..), curry, fst, snd)
-import Data.Tuple.Nested ((/\))
-
-type Iso p a b = { fwd :: p a b, bwd :: p b a }
-
-class Bifunctor t <= Associative t p
-  where
-  assoc :: forall a b c. Iso p (t a (t b c)) (t (t a b) c)
-
-instance associativeCartesian :: Associative Tuple (->)
-  where
-  assoc =
-    { fwd: \(a /\ b /\ c) -> (a /\ b) /\ c
-    , bwd: \((a /\ b) /\ c) -> a /\ b /\ c
-    }
-
-instance associativeCocartesian :: Associative Either (->)
-  where
-  assoc =
-    { fwd: either (Left <<< Left) (either (Left <<< Right) Right)
-    , bwd: either (either Left (Right <<< Left)) (Right <<< Right)
-    }
-
-class Associative t p <= Tensor t i p
-  where
-  lunit :: forall a. Iso p (t i a) a
-  runit :: forall a. Iso p (t a i) a
-
-instance tensorCartesian :: Tensor Tuple Unit (->)
-  where
-  lunit = { fwd: snd, bwd: Tuple unit }
-  runit = { fwd: fst, bwd: flip Tuple unit }
-
-instance tensorCocartesian :: Tensor Either Void (->)
-  where
-  lunit = { fwd: either absurd identity, bwd: Right }
-  runit = { fwd: either identity absurd, bwd: Left }
+import Data.Tuple (Tuple, curry)
 
 class (Associative l c, Associative r c, Profunctor p) <= Semigroupal c l r o p
   where
-  pzip :: forall t u v w.
-    c (o (p t u) (p v w)) (p (l t v) (r u w))
+  pzip :: forall d e f g.
+    c (o (p d e) (p f g)) (p (l d f) (r e g))
 
 pzipInfix :: forall l r p a b c d. Semigroupal (->) l r Tuple p => p a b -> p c d -> p (l a c) (r b d)
 pzipInfix = curry pzip
 
-infixr 5 pzipInfix as :&:
+infixr 5 pzipInfix as &&&
 
 class Profunctor p <= Unital c l r o p
   where
