253ee6808ae25e6f82cad18a145cf71eb9221055
Asad Saeeduddin <masaeedu@gmail.com>
2020-03-22 14:29:56 -0400

    Add monomorphized operators

--- src/Data/Profunctor/Monoidal.purs
+++ src/Data/Profunctor/Monoidal.purs
@@ -2,17 +2,37 @@ module Data.Profunctor.Monoidal where
 
 import Control.Category.Tensor (class Associative, class Tensor)
 import Data.Profunctor (class Profunctor)
+
 import Data.Tuple (Tuple, curry)
+import Data.Tuple.Nested (type (/\))
+
+import Data.Either (Either)
+import Data.Either.Nested (type (\/))
 
 class (Associative l c, Associative r c, Associative o c, Profunctor p) <= Semigroupal c l r o p
   where
   pzip :: forall d e f g.
     c (o (p d e) (p f g)) (p (l d f) (r e g))
 
-pzipInfix :: forall l r p a b c d. Semigroupal (->) l r Tuple p => p a b -> p c d -> p (l a c) (r b d)
-pzipInfix = curry pzip
+mux :: forall p a b c d. Semigroupal (->) Tuple Tuple Tuple p => p a b -> p c d -> p (a /\ c) (b /\ d)
+mux = curry pzip
 
-infixr 5 pzipInfix as &&&
+infixr 5 mux as &&
+
+demux :: forall p a b c d. Semigroupal (->) Either Either Tuple p => p a b -> p c d -> p (a \/ c) (b \/ d)
+demux = curry pzip
+
+infixr 4 demux as ||
+
+switch :: forall p a b c d. Semigroupal (->) Tuple Either Tuple p => p a b -> p c d -> p (a /\ c) (b \/ d)
+switch = curry pzip
+
+infixr 5 switch as &|
+
+splice :: forall p a b c d. Semigroupal (->) Either Tuple Tuple p => p a b -> p c d -> p (a \/ c) (b /\ d)
+splice = curry pzip
+
+infixr 5 splice as |&
 
 class Profunctor p <= Unital c l r o p
   where
