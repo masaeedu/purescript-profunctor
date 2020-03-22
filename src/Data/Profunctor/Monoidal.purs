3a9fb4082837aecfef6a780cc0ff5539c45efa1b
Asad Saeeduddin <masaeedu@gmail.com>
2020-03-22 15:06:34 -0400

    Add some monomorphized punit aliases for various tensor combinations

--- src/Data/Profunctor/Monoidal.purs
+++ src/Data/Profunctor/Monoidal.purs
@@ -1,13 +1,13 @@
 module Data.Profunctor.Monoidal where
 
+import Prelude
+
 import Control.Category.Tensor (class Associative, class Tensor)
-import Data.Profunctor (class Profunctor)
-
-import Data.Tuple (Tuple, curry)
-import Data.Tuple.Nested (type (/\))
-
 import Data.Either (Either)
 import Data.Either.Nested (type (\/))
+import Data.Profunctor (class Profunctor, dimap, lcmap, rmap)
+import Data.Tuple (Tuple, curry)
+import Data.Tuple.Nested (type (/\))
 
 class (Associative l c, Associative r c, Associative o c, Profunctor p) <= Semigroupal c l r o p
   where
@@ -37,5 +37,17 @@ infixr 5 splice as |&
 class Profunctor p <= Unital c l r o p
   where
   punit :: c o (p l r)
+
+terminal :: forall p a. Unital (->) Unit Unit Unit p => p a Unit
+terminal = lcmap (const unit) $ punit unit
+
+initial :: forall p a. Unital (->) Void Void Unit p => p Void a
+initial = rmap absurd $ punit unit
+
+poly :: forall p a b. Unital (->) Unit Void Unit p => p a b
+poly = dimap (const unit) absurd $ punit unit
+
+mono :: forall p. Unital (->) Void Unit Unit p => p Void Unit
+mono = punit unit
 
 class (Tensor l il c, Tensor r ir c, Tensor o io c, Semigroupal c l r o p, Unital c il ir io p) <= Monoidal c l il r ir o io p
